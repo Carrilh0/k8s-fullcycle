02d60491151d36020d526e718589549bde6dfbdd
Vitor Carrilho <vitor.carrilho@hotmail.com>
2022-11-01 00:10:01 -0300

    aprendendo a usar volumes no k8s

--- server.go
+++ server.go
@@ -2,12 +2,15 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
+	"log"
 	"net/http"
 	"os"
 )
 
 func main() {
 	http.HandleFunc("/", Hello)
+	http.HandleFunc("/configmap", ConfigMap)
 	http.ListenAndServe(":8000", nil)
 }
 
@@ -15,4 +18,14 @@ func Hello(w http.ResponseWriter, r *http.Request) {
 	name := os.Getenv("NAME")
 	age := os.Getenv("AGE")
 	fmt.Fprintf(w, "Hello! i'm %s. I'm %s", name, age)
+}
+
+func ConfigMap(w http.ResponseWriter, r *http.Request) {
+	data, err := ioutil.ReadFile("myfamily/family.txt")
+
+	if err != nil {
+		log.Fatalf("Error reading file: ", err)
+	}
+
+	fmt.Fprintf(w, "My Family: %s", string(data))
 }
